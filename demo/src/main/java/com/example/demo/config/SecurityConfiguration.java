af1faa64ae37576da84d0ced7eb9e9872fe4d3a0
crouvel <55972660+crouvel@users.noreply.github.com>
2022-11-22 14:20:55 +0100

    fix login

--- demo/src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -1,19 +1,15 @@
 package com.example.demo.config;
 
-import com.example.demo.service.AuthenticationFilter;
-import com.example.demo.service.LoginFilter;
 import com.example.demo.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -22,7 +18,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
     @Autowired
     private UserDetailsServiceImpl customUserDetailsService;
@@ -33,16 +29,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().and().authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/login").permitAll()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().antMatchers("/login").permitAll()
+                //.antMatchers("/users/**", "/settings/**").hasAuthority("Admin")
                 .anyRequest().authenticated()
+                .and().formLogin()
+                //.loginPage("/login")
+                .usernameParameter("email")
+
+                .permitAll()
                 .and()
-                .addFilterBefore(new LoginFilter("/login", authenticationManager()),
-                        UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new AuthenticationFilter(),
-                        UsernamePasswordAuthenticationFilter.class);
+                .rememberMe().key("AbcdEfghIjklmNopQrsTuvXyz_0123456789")
+                .and()
+                .logout().permitAll();
+
+        http.headers().frameOptions().sameOrigin();
+
+        return http.build();
     }
 
     @Bean
