617cb167c8f13ccb50fb53d8517b0f6149db9f21
crouvel <55972660+crouvel@users.noreply.github.com>
2022-11-30 00:35:26 +0100

    route changes for controller

--- demo/src/main/java/com/example/demo/controller/ChercheurController.java
+++ demo/src/main/java/com/example/demo/controller/ChercheurController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 public class ChercheurController {
     @Autowired
     private ChercheurRepository chercheurRepository;
--- demo/src/main/java/com/example/demo/controller/EmployeurController.java
+++ demo/src/main/java/com/example/demo/controller/EmployeurController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 public class EmployeurController {
 
     @Autowired
